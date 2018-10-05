78d3b98931e8fed55af1ef07e77ea170b8399c4a
jackmalhotra <43881062+jackmalhotra@users.noreply.github.com>
2018-10-05 17:50:28 +0530

    Add files via upload

---
+++ home.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Hyperloop India | Home</title>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="./style/home.css">
+	<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+	
+</head>
+<body>
+
+	<nav class="branding">
+		<div>
+			<h1>
+				<img src="./img/logo.png" width="225" height="42">
+			</h1>
+		</div>
+		<ul>
+			<li><a href="home.php"><i class="fa fa-fw fa-home"></i>Home</a></li>
+			<li><a href="http://hyperloopindia-hyperloop-india.193b.starter-ca-central-1.openshiftapps.com/">Our Technology</a></li>
+			<li><a href="home.php#about-us"><i class="fa fa-fw fa-envelope"></i>Contact</a></li>
+			<li><a href="login.php"><i class="fa fa-fw fa-user"></i>Login</a></li>
+		</ul>
+	</nav>
+
+
+
+	<section id="showcase">
+		<h1>Welcome To Hyperloop India</h1>
+		<h2>Mumbai - Delhi - Banglore - Pune</h2>
+		<a href="login.php" class="button">Book A Train</a>
+	</section>
+
+	<div id="about-us" class="about-us">
+		<div class="about-us-a">
+			<h2>Links</h2>
+			<p>Blogs</p>
+		</div>
+		<div class="about-us-a">
+			<h2>Contact</h2>
+			<p>+91-8286835308</p>
+			<p>+91-8286835308</p>
+			<p>+91-8286835308</p>
+		</div>
+		<div class="about-us-a">
+			<h2>Follow Us</h2><br>
+			<a href="https://www.facebook.com/"><i class="fa fa-facebook-square" style="font-size:32px"></i></a>
+			<a href="https://www.instagram.com/"><i class="fa fa-instagram" style="font-size:32px"></i></a>
+			<a href="https://www.linkedin.com/feed/?trk=onboarding-landing"><i class="fa fa-linkedin" style="font-size:32px"></i></a>
+			<a href="https://twitter.com/"><i class="fa fa-twitter" style="font-size:32px"></i></a>
+			
+		
+			
+		</div>
+	</div>
+
+	<section id="copyright">
+		<p>© 2017 Hyperloop India/ Terms & Conditions / Privacy Policy</p>
+	</section>
+</body>
+</html>
